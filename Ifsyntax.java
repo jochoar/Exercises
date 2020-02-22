dae72d5db2438157e1bcf040d73ccc17298f367b
jonathan ochoa restrepo <j.ochoar26@gmail.com>
2020-02-22 18:47:05 -0500

    practicing if syntax

---
+++ Ifsyntax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ifsyntax {
+
+    public static void main(String [] args){
+       
+        Scanner leer = new Scanner (System.in);
+        System.out.println("Cual es tu calificacion? ");
+
+        double calificacion = leer.nextDouble();
+    
+        if(calificacion >= 6){
+            System.out.println("Has aprobado");
+        }
+        
+        if (calificacion < 6){
+            System.out.println("Has reprobado");
+        }
+
+    }
+
+}
