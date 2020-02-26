c4cd6b0aea715f83aa5fa3925defcd3e7acdb8d3
jonathan ochoa restrepo <j.ochoar26@gmail.com>
2020-02-25 21:04:34 -0500

    Exercise with the FOR structure.

---
+++ Estructuras de control/TableFor.java
@@ -0,0 +1,17 @@
+
+
+public class TableFor {
+
+    public static void main(String[] args) {
+        int table = 7;
+
+        for (int x = 1; x <=10; x++ ){
+
+            int multiplication = table * x;
+
+            System.out.println(table + "x" + x + "=" + multiplication);
+        }
+
+        System.out.println("Termino");
+    }
+}
